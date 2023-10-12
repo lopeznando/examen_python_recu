8a264c36a091bfef074dab882b58c5107392070f
Nando Lopez <nandolopezsantaria@gamil.com>
2023-10-12 10:08:17 -0500

    examen de recu

--- src/main.py
+++ src/main.py
@@ -3,16 +3,23 @@
 
 # 1. la funcion recibe como parametro un array y devera devolver el numero mayor del array
 def numero_mayor(arr):
-  pass
+  mayor=0
+  for num in arr:
+   if num >= mayor:
+    mayor = num
+  return mayor
 
 # 2. la funcion recibe un string la funcion devera devolver el string ivertido ejem:entrada=hola, devuleve=aloh
 def inverso(txt):
-  pass
+    return txt[::-1]
 
-# 3. la funcion recibe un string y devuelve si la plabara es un palindromo True si no devuelve False: palindromo que se lee igual de derecha a izquierda y viceversa ejm: entrada=reconocer es palindromo se le igual de izquierda a derecha y viciversa
+# 3. la funcion recibe un string y devuelve si la plabara es un palindromo True si no devuelve False: palindromo que se lee igual de derecha a izquierda y viceversa ejm: entrada=reconocer es palindromo se lee igual de izquierda a derecha y viciversa
 def palindromo(txt):
-  pass
-
+   return txt == txt[::-1]
 # 4. la funcion recibe como parametro una letra y devuelve si la letra ingresada es vocal o consonante el mensaje devera ser si es vocal = 'es vocal' si es consonante 'es consonante'
 def es_vocal(letra):
-  pass
+ vocales = ['a', 'e', 'i', 'o', 'u'] 
+ if letra in vocales:
+        return 'es vocal'
+ else:
+        return 'es consonante'
